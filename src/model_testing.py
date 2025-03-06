5f47f1c12ac05719229155b69e00e926df9a8d3e
Rahultawar <iamrahultawar@gmail.com>
2025-03-06 11:04:03 +0530

    Updated model_testing.py

--- src/model_testing.py
+++ src/model_testing.py
@@ -1,28 +1,34 @@
+import os
+import cv2
+import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
+import joblib
+import mediapipe as mp
+from utils import preprocess_image, load_data  
 
 def load_trained_model(model_dir):
-    # Load model weights
-    model = load_model(f"{model_dir}/trained_model.h5")
-    
     # Load model architecture
     with open(f"{model_dir}/model_architecture.json", "r") as f:
         model_architecture = f.read()
     model = tf.keras.models.model_from_json(model_architecture)
+    
+    # Load model weights
+    model.load_weights(f"{model_dir}/trained_model.h5")
     
     # Compile the model
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     
     return model
 
-def test_model(model, test_data_file):
-    df = pd.read_csv(test_data_file)
-    X_test = df.drop('label', axis=1).values
-    y_test = pd.get_dummies(df['label']).values
+def test_model(model, data_dir, scaler):
+    X_test, y_test = load_data(data_dir, scaler)
     
-    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+    # Convert labels to one-hot encoding
+    y_test = pd.get_dummies(y_test).values
     
     # Make predictions
     y_pred = model.predict(X_test)
@@ -38,6 +44,7 @@ def test_model(model, test_data_file):
 
 if __name__ == "__main__":
     model_dir = "models"
-    test_data_file = "data/test/test_data.csv"  # Use the saved test data file
+    test_data_dir = "data/test"  # Directory containing test images
+    scaler = joblib.load(os.path.join(model_dir, 'scaler.pkl'))
     model = load_trained_model(model_dir)
-    test_model(model, test_data_file)
+    test_model(model, test_data_dir, scaler)
